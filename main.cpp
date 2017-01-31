8093056df94d32f23b6e64d7e8bc329f635cfa29
Felipe Gustavo de Souza Gomes <felipegustavo1000@gmail.com>
2017-01-31 03:02:52 -0300

    Points added to the view

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <vector>
+#include <array>
 
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
@@ -7,8 +9,25 @@
 int mouse_position_x;
 int mouse_position_y;
 
-static void error_callback(int error, const char* description) {
+std::vector<std::array<float, 2>> points_array;
+
+void error_callback(int error, const char* description) {
     fprintf(stderr, description);
+}
+
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (action != GLFW_PRESS) {
+        return;
+    }
+
+    if (key == GLFW_KEY_ESCAPE) {
+        glfwSetWindowShouldClose(window, GL_TRUE);
+    } else if (key == GLFW_KEY_E) {
+        points_array.clear();
+    }
+}
+
+void character_callback(GLFWwindow* window, unsigned int codepoint) {
 }
 
 void mouse_button_callback(GLFWwindow* window, int button, int action, int mods) {
@@ -23,21 +42,41 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
     float virtualX = (mouse_position_x * 1.0f / width * 2 - 1);
     float virtualY = (-mouse_position_y * 1.0f / height * 2 + 1);
 
+    points_array.push_back({virtualX, virtualY});
 }
 
-static void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods) {
-    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
-        glfwSetWindowShouldClose(window, GL_TRUE);
+void draw_points() {
+    glEnable(GL_POINT_SMOOTH);
+    glPointSize(3.0f);
+
+    glBegin(GL_POINTS);
+    for (std::vector<std::array<float, 2>>::iterator it = points_array.begin(); it != points_array.end(); ++it) {
+        glVertex2f((*it)[0], (*it)[1]);
     }
+    glEnd();
+
+    glDisable(GL_POINT_SMOOTH);
 }
 
-static void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
+void draw_lines() {
+    int threshold = points_array.size() - 1;
+    int index = 0;
+
+    glBegin(GL_LINES);
+    for (std::vector<std::array<float, 2>>::iterator it = points_array.begin(); it != points_array.end(); ++it) {
+        if (index > 0 && index < threshold) {
+           glVertex2f((*it)[0], (*it)[1]); 
+        }
+        glVertex2f((*it)[0], (*it)[1]);
+        index++;
+    }
+    glEnd();
+}
+
+void cursor_position_callback(GLFWwindow* window, double xpos, double ypos) {
     mouse_position_x = xpos;
     mouse_position_y = ypos;
-}
-
-static void character_callback(GLFWwindow* window, unsigned int codepoint) {
-}
+}  
 
 int main() {
     GLFWwindow* window;
@@ -48,26 +87,17 @@ int main() {
         exit(EXIT_FAILURE);
     }
 
-    glfwWindowHint(GLFW_SAMPLES, 4);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
 
     // window = glfwCreateWindow(640, 480, "Computação Visual - Atividade 1", glfwGetPrimaryMonitor(), NULL);
-    window = glfwCreateWindow(640, 480, "Computação Visual - Atividade 1", NULL, NULL);
+    window = glfwCreateWindow(960, 680, "Computação Visual - Atividade 1", NULL, NULL);
 
-    if (window == NULL) {
+    if (!window) {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
 
     glfwMakeContextCurrent(window);
-    glewExperimental = true;
-
-    if (glewInit() != GLEW_OK) {
-        fprintf(stderr, "Failed to initialize GLEW\n");
-        exit(EXIT_FAILURE);
-    }
 
     glfwSetKeyCallback(window, key_callback);
     glfwSetCursorPosCallback(window, cursor_position_callback);
@@ -75,6 +105,11 @@ int main() {
     glfwSetCharCallback(window, character_callback);
 
     while (!glfwWindowShouldClose(window)) {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        draw_points();
+        draw_lines();
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
